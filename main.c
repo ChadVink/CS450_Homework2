2bd62a2e8a375db0e27e6966229c487a4eaa3af5
Chad Vink <ChadVink@Chads-MacBook-Pro.local>
2016-02-28 14:24:22 -0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "constants.h"
+#include "parsetools.h"
+#include <sys/wait.h>
+
+int main() {
+    // Buffer for reading one line of input
+    char line[MAX_LINE_CHARS];
+    char* line_words[MAX_LINE_WORDS + 1];
+
+    /* Make character array to hold command */
+
+    // Loop until user hits Ctrl-D (end of input)
+    // or some other input error occurs
+    while( fgets(line, MAX_LINE_CHARS, stdin) ) {
+        int num_words = split_cmd_line(line, line_words);
+
+        // Just for demonstration purposes
+        for (int i=0; i < num_words; i++){
+            printf("%s\n", line_words[i]);
+            if ( line_words[i] == )
+                printf("pipe");
+        }
+
+    }
+
+    return 0;
+}
+
+
+/*
+ */
