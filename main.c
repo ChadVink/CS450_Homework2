653b25462dc4e137b29f01b1b8750ba4d84c6a32
Chad Vink <ChadVink@Chads-MacBook-Pro.local>
2016-03-09 15:02:02 -0800

    Take user input then run the command when the line in finished

--- main.c
+++ main.c
@@ -19,8 +19,8 @@ void syserror(const char* s);
 //int CreateProcess(struct command * cmd, int input, int output);
 //int PipeFork(int n, struct command * cmd);
 
-int PipeFork(int n, struct command *cmd);
-int CreateProcess (int in, int out, struct command *cmd);
+int PipeFork(int n,char* cmdArray[][MAX_LINE_WORDS+1]);
+int CreateProcess (int in, int out, char* cmd[]);
 
 
 
@@ -31,7 +31,8 @@ int main() {
     int countpipes; // Number of pipes that are read in
     int num_words;
     int idx; // index of the word on the line_words
-        //char* currentCommand[MAX_LINE_WORDS + 1];
+    char* commandArray[MAX_LINE_WORDS + 1][MAX_LINE_WORDS + 1];
+
 
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
@@ -40,13 +41,10 @@ int main() {
         idx = 0;
         num_words = split_cmd_line(line, line_words);
         int cmdCount = 0;
-        char* commandArray[MAX_LINE_WORDS + 1][MAX_LINE_WORDS + 1];
+        //char* commandArray[MAX_LINE_WORDS + 1][MAX_LINE_WORDS + 1];
 
-        // ls -l -a | less
-        // { { ls, -l, -a }, {less} }
 
          while( idx < num_words ){
-            //char* currentCommand[MAX_LINE_WORDS + 1];
             if(strcmp(line_words[idx], "|") != 0){ // if line_words[idx] is not a "|"
                 commandArray[countpipes][cmdCount] = line_words[idx];
                 cmdCount++;
@@ -58,22 +56,8 @@ int main() {
              }
             idx++;
         }
-           //printf("im out\n");
-            /*if (idx == 0){
-                char* currentCommand[MAX_LINE_WORDS + 1] = {};
-            }*/
 
-            //if (strcmp(line_words[idx], "|") == 0){
-                //currentCommand[++cmdCount] = 0;
-    		//commandArray[countpipes++] = currentCommand;
-                //cmdCount = 0;
-              //  char* currentCommand[MAX_LINE_WORDS +1] = {};
-            //}else{
-             //   currentCommand[cmdCount++] = line_words[idx];
-            //}
-
-
-        int j;
+       /* int j;
         for( int i = 0; i < countpipes+1; i++ ){
             j = 0;
             while(commandArray[i][j] != 0){
@@ -81,12 +65,14 @@ int main() {
                 j++;
             }
             printf("\n");
-        }
-
+        }*/
+        PipeFork(countpipes+1, commandArray);
+    }
+    
+       printf("here");
 
         //struct command cmd[] = currentCommand;
-        //PipeFork(countpipes, cmd);
-
+        
 
          /*for (int i=0; i < num_words; i++){
 	    if (strcmp(line_words[i], "|") == 0){
@@ -96,8 +82,6 @@ int main() {
             printf("%s\n", line_words[i]);
 	}
         printf("%d\n", countpipes);*/
-    }
-
 
 //		const char *who[] = { "who", 0};
 //		const char *wc[] = {"wc", "-l", 0};
@@ -130,7 +114,7 @@ void syserror(const char *s)
     exit( 1 );
 }
 
-int CreateProcess(int input, int output, struct command *cmd)
+int CreateProcess(int input, int output, char* cmd[] )
 {
 
   pid_t pid;
@@ -150,41 +134,45 @@ int CreateProcess(int input, int output, struct command *cmd)
           close(output);
         }
 
-      return execvp(cmd->parameters[0], (char * const *)cmd->parameters);
+      return execvp(cmd[0], cmd);
     }
   /* parent returns pid, should not execute */
   return pid;
 }
 
-int PipeFork(int params, struct command *cmd)
+int PipeFork(int params, char* cmdArray[][MAX_LINE_WORDS+1])
 {
   int i;
   pid_t pid;
   int input;
   int fd[2];
 
+  if ((pid = fork()) == 0){
 
-  /* first process input end is original fd[0]*/
-  input = 0;
+      /* first process input end is original fd[0]*/
+      input = 0;
 
-  /* pipe EVERYTHING EXCEPT the final pipe here */
-  for (i = 0; i < params - 1; ++i)
-    {
-      pipe(fd);
+      /* pipe EVERYTHING EXCEPT the final pipe here */
+      for (i = 0; i < params - 1; i++)
+        {
+          pipe(fd);
 
-      /* write to fd[1], in is transmitted from previous stage of pipe*/
-      CreateProcess(input, fd [1], cmd + i);
-      /* close write end of pipe and save read end of pipe! */
-      close(fd [1]);
-      input = fd[0];
-    }
+          /* write to fd[1], in is transmitted from previous stage of pipe*/
+          CreateProcess(input, fd [1], cmdArray[i]);
+          /* close write end of pipe and save read end of pipe! */
+          close(fd [1]);
+          input = fd[0];
+        }
 
-  /* set stdin as read end of prev pipe and output this to fd*/
-  if (input != 0)
-    dup2(input, 0);
+      /* set stdin as read end of prev pipe and output this to fd*/
+      if (input != 0)
+        dup2(input, 0);
 
-  /* exec the command */
-  return execvp(cmd[i].parameters[0], (char * const *)cmd[i].parameters);
+      /* exec the command */
+      return execvp(cmdArray[i][0], cmdArray[i]);
+
+  }
+  return pid;
 }
 
 
