a0fefac54340574882d0aa4e2a0daa08c008bab4
NShively <njshively1354@gmail.com>
2016-03-10 00:59:59 -0800

    Final changes
    Couldn't get multiple redirects running. Added error handling.

---
+++ README.txt
@@ -0,0 +1,10 @@
+Hey Dr. Rivoire,
+
+Our shell has a bug. The < redirect cannot be used in conjunction with other redirects
+or pipes. We all spent a long time trying to fix this, but we were unable to fix our code.
+It's odd, because we're doing nearly the same process as we do with the > redirect, which 
+works with other pipes and redirects, but we cannot replicate this with <. The reason we
+did not ask about it in class is because we figured with the extra 24 hours deadline, we
+would be able to figure it out. However, after hours of tinkering, we are still unsuccessful
+in this bug fix.
+
--- main.c
+++ main.c
@@ -11,14 +11,9 @@
 
 void syserror(const char* s);
 
-//int CreateProcess(struct command * cmd, int input, int output);
-//int PipeFork(int n, struct command * cmd);
-
-int PipeFork(int n,char* cmdArray[][MAX_LINE_WORDS+1], int* order);
+int PipeFork(int n,char* cmdArray[][MAX_LINE_WORDS+1], int* PipeType, int RedirectsFound);
 int CreateProcess (int in, int out, char* cmd[]);
-int CreateRedirect(int input, int output, char* cmd[], int type, char* fileloc[]);
-
-
+int CreateRedirect(int input, int output, char* cmd[], int type, char* FileName[]);
 
 int main() {
     // Buffer for reading one line of input
@@ -33,30 +28,33 @@ int main() {
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
     while( fgets(line, MAX_LINE_CHARS, stdin) ) {
-        countpipes = 0;
+        int RedirectsFound = 0;
+	countpipes = 0;
         idx = 0;
-	int order[100];
+	int PipeType[100];
         num_words = split_cmd_line(line, line_words);
         int cmdCount = 0;
         memset(commandArray, 0, sizeof commandArray);
-        //char* commandArray[MAX_LINE_WORDS + 1][MAX_LINE_WORDS + 1] = {};
-
 
          while( idx < num_words ){
 	    int check1 = strcmp(line_words[idx], "|");
 	    int check2 = strcmp(line_words[idx], ">");
 	    int check3 = strcmp(line_words[idx], "<");
-            if(strcmp(line_words[idx], "|") != 0 && check2 != 0 && check3 != 0){ // if line_words[idx] is not a "|"
+            if(check1 != 0 && check2 != 0 && check3 != 0){
+		 // if line_words[idx] is not a "|", ">", or "<"
                 commandArray[countpipes][cmdCount] = line_words[idx];
                 cmdCount++;
             }
             else{
+		// check which type of pipe operation should be performed
+		// 1 is pipe, 2 and 3 are redirects.
+		// Store these in an array in sequential order
 		if (check1 == 0)
-			order[countpipes] = 0;
-		if (check2 == 0)
-			order[countpipes] = 1;
-		if (check3 == 0)
-			order[countpipes] = 2;
+			PipeType[countpipes] = 0;
+		if (check2 == 0){
+			PipeType[countpipes] = 1; RedirectsFound++;}
+		if (check3 == 0){ RedirectsFound++;
+			PipeType[countpipes] = 2;}
 
                 commandArray[countpipes][cmdCount+1] = 0;
                 cmdCount = 0;
@@ -65,48 +63,9 @@ int main() {
             idx++;
         }
 
-       /* int j;
-        for( int i = 0; i < countpipes+1; i++ ){
-            j = 0;
-            while(commandArray[i][j] != 0){
-                printf("%s ", commandArray[i][j]);
-                j++;
-            }
-            printf("\n");
-        }*/
-        PipeFork(countpipes+1, commandArray, order);
+        PipeFork(countpipes+1, commandArray, PipeType, RedirectsFound);
     }
     
-   //    printf("here");
-
-        //struct command cmd[] = currentCommand;
-        
-
-         /*for (int i=0; i < num_words; i++){
-	    if (strcmp(line_words[i], "|") == 0){
-    		countpipes++;
-            }
-            printf("%s\n", line_words[i]);
-	}
-        printf("%d\n", countpipes);*/
-
-//		const char *who[] = { "who", 0};
-//		const char *wc[] = {"wc", "-l", 0};
-	//	const char *ls[] = {"ls", "-l", 0};
-	//	const char *awk[] = {"awk", "{print $1}", 0 };
-	//	const char *sort[] = {"sort", 0};
-	//	const char *uniq[] = {"uniq", 0};
-//		struct command cmd[] = { {who}, {wc}};
-		//struct command cmd[2];
-		//cmd[0].parameters = who;
-		//cmd[1].parameters = wc;
-		// pass the number of commands to PipeFork as well as
-		// the cmd struct with a char** array pointing to
-		// array of command/parameters. Above is an example in hard
-		// code. We have to take all the inputs from the user and pass
-		// them to the PipeFork command.
-		//return PipeFork(2, cmd);
-	//printf("pipes: %d\n", countpipes);
 	return 0;
 }
 
@@ -122,32 +81,46 @@ void syserror(const char *s)
 }
 
 
-int CreateRedirect(int input, int output, char* cmd[], int type, char* fileloc[]){
+int CreateRedirect(int input, int output, char* cmd[], int type, char* FileName[]){
 
 	pid_t pid;
 	if(type == 2){
+	// < redirect
 		if ((pid = fork()) == 0)
 		{
-			int fdIn = open(fileloc[0], O_RDONLY, 0666);
+//			dup2(input, 1);
+			int fdIn;
+			if ((fdIn = open(FileName[0], O_RDONLY, 0666)) == -1)
+				syserror("Could not open file in < redirect");
 			dup2(fdIn, 0);
-			close(fdIn);
-//			int fdOut = output;
-//			dup2(fdOut, 1);
+			if(close(fdIn) == -1)
+				syserror("Could not close fdin in < redirect");
 			return execvp(cmd[0], cmd);
+			syserror("Could not execute < redirect");
 		}
+		else if (pid == -1)
+			syserror("could not fork < redirect");
 	}
 	else{
+	// > redirect
 		if ((pid = fork()) == 0)
 		{
 			int fdIn = output;
 			dup2(fdIn, 0);
-			int fdOut = open(fileloc[0], O_WRONLY|O_CREAT, 0666);
+			int fdOut;
+			if( (fdOut = open(FileName[0], O_WRONLY|O_CREAT, 0666)) == -1)
+				syserror("Could not open file in > redirect");
 			dup2(fdOut,1);
-			close(fdOut);
-			close(fdIn);
-
+			if(close(fdOut) == -1)
+				syserror("Could not close fdout in > redirect");
+			if(close(fdIn) == -1)
+				syserror("Could not close fdin in > redirect");
+			
 			return execvp(cmd[0], cmd);
+			syserror("Could not execute > redirect"); 
 		}
+		else if (pid == -1)
+			syserror("could not create fork for > redirect");
 	}
 }
 
@@ -162,57 +135,65 @@ int CreateProcess(int input, int output, char* cmd[] )
       if (input != 0)
         {
           dup2(input, 0);
-          close(input);
+          if(close(input) == -1)
+		syserror("Could not close input in CreateProcess");
         }
 
       if (output != 1)
         {
           dup2(output, 1);
-          close(output);
+         if( close(output) == -1)
+		syserror("Could not close output in CreateProcess");
         }
 
       return execvp(cmd[0], cmd);
+	syserror("Could not execute pipe!");
     }
+  else if (pid == -1)
+	syserror("Could not create fork in CreateProcess");
   /* parent returns pid, should not execute */
   return pid;
 }
 
-int PipeFork(int params, char* cmdArray[][MAX_LINE_WORDS+1], int* order)
+int PipeFork(int params, char* cmdArray[][MAX_LINE_WORDS+1], int* PipeType, int TotalRedirects)
 {
   int i;
   pid_t pid;
   int input;
   int fd[2];
-
   if ((pid = fork()) == 0){
-
+      int RedirectsFound = 0;
       /* first process input end is original fd[0]*/
       input = 0;
-
       /* pipe EVERYTHING EXCEPT the final pipe here */
       for (i = 0; i < params - 1; i++)
         {
-          pipe(fd);
+         if ( pipe(fd) == -1 )
+		syserror("Pipe failure");
 
           /* write to fd[1], in is transmitted from previous stage of pipe*/
-        if (order[i] == 0) 
-	 CreateProcess(input, fd [1], cmdArray[i]);
+        if (PipeType[i] == 0) {
+	 CreateProcess(input, fd[1], cmdArray[i]);
+	}
           /* close write end of pipe and save read end of pipe! */
-         if (order[i] == 1 || order[i] == 2)
-		CreateRedirect(fd[1], input, cmdArray[i], order[i], cmdArray[i+1]);
-
-	  close(fd [1]);
+         if (PipeType[i] == 1 || PipeType[i] == 2){
+		RedirectsFound++;
+		CreateRedirect(fd[1], input, cmdArray[i], PipeType[i], cmdArray[params-RedirectsFound]);
+	}	
+	  if(close(fd [1]) == -1)
+		syserror("Could not close fd[1]");
           input = fd[0];
         }
 	
-
-      /* set stdin as read end of prev pipe and output this to fd*/
-      if (input != 0)
+	
+      if (input != 0 && PipeType[params] == 0)
         dup2(input, 0);
 
       /* exec the command */
       return execvp(cmdArray[i][0], cmdArray[i]);
 
   }
+  else if (pid == -1)
+	syserror("Fork Failure");
   return pid;
 }
